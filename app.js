4e632baa324d83357da395b0e3c044f9a1693fa7
goyal <goyalhimanshu079@gmail.com>
2022-02-04 16:52:54 +0530

    pus2

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ import express from "express";
 const app = express();
 const port = 5000;
 app.get("/", (req, res) => {
-  res.send("<h1>Hello Everyone from JENKINS</h1>");
+  res.send("<h1>Hello Everyone from JENKINS 22</h1>");
 });
 app.listen(port);
